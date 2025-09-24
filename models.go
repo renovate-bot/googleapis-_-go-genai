ba5c3834d2489db981fc70ca7c8d2922d3dc3c11
Kaituo Huang <kaituo@google.com>
2025-09-23 17:30:43 -0700

    feat: Add FunctionResponsePart & ToolComputerUse.excludedPredefinedFunctions
    FunctionResponsePart could be used to support the computer use tool with browser screenshot
    
    PiperOrigin-RevId: 810641991

--- models.go
+++ models.go
@@ -4687,6 +4687,11 @@ func toolComputerUseToMldev(fromObject map[string]any, parentObject map[string]a
 		setValueByPath(toObject, []string{"environment"}, fromEnvironment)
 	}
 
+	fromExcludedPredefinedFunctions := getValueByPath(fromObject, []string{"excludedPredefinedFunctions"})
+	if fromExcludedPredefinedFunctions != nil {
+		setValueByPath(toObject, []string{"excludedPredefinedFunctions"}, fromExcludedPredefinedFunctions)
+	}
+
 	return toObject, nil
 }
 
@@ -4696,6 +4701,11 @@ func toolComputerUseToVertex(fromObject map[string]any, parentObject map[string]
 	fromEnvironment := getValueByPath(fromObject, []string{"environment"})
 	if fromEnvironment != nil {
 		setValueByPath(toObject, []string{"environment"}, fromEnvironment)
+	}
+
+	fromExcludedPredefinedFunctions := getValueByPath(fromObject, []string{"excludedPredefinedFunctions"})
+	if fromExcludedPredefinedFunctions != nil {
+		setValueByPath(toObject, []string{"excludedPredefinedFunctions"}, fromExcludedPredefinedFunctions)
 	}
 
 	return toObject, nil
